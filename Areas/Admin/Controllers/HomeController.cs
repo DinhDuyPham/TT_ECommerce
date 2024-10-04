47d1a500e1c51de2084a049eb15f0d3397836dba
TTTai12 <tientantai12@gmail.com>
2024-10-04 16:10:48 +0700

    thay doi layout admin

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ namespace TT_ECommerce.Areas.Admin.Controllers
     [Area("Admin")] // Correct attribute for areas
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
+     
     }
 }
