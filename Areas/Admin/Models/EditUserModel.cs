b4ae7a36c01158784e11ad632d13d281c156eeb0
lyrexiv <2100008358@nttu.edu.vn>
2024-10-05 19:46:02 +0700

    admin

---
+++ Areas/Admin/Models/EditUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TT_ECommerce.Areas.Admin.Models
+{
+    public class EditUserModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
+
