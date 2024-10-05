eedf7cdf41fa5319a23a7d919654b40fba28e0cd
DinhDuyPham <112776674+DinhDuyPham@users.noreply.github.com>
2024-10-05 23:33:19 +0700

    feature Controller/View ProductDetail Fix page redirection from product to  productdetail  and add function

--- Controllers/ProductDetailsController.cs
+++ Controllers/ProductDetailsController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TT_ECommerce.Models; // Import namespace chứa model Product
+using System.Linq;
+using TT_ECommerce.Data;
 
 namespace TT_ECommerce.Controllers
 {
     public class ProductDetailsController : Controller
     {
-        public IActionResult Index()
+        private readonly TT_ECommerceDbContext _context; // ApplicationDbContext là DbContext của bạn
+
+        public ProductDetailsController(TT_ECommerceDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(int id)
+        {
+            // Lấy sản phẩm theo id từ cơ sở dữ liệu
+            var product = _context.TbProducts.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Truyền product vào view
+            return View(product);
         }
     }
 }
